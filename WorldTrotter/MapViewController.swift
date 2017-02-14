4f4d50b3f109e1504ae48b46731cec016f3f29fe
Alex Louzao <alexlouzao@desktop-umpqin0.highpoint.edu>
2017-02-13 21:30:13 -0500

    Location and Pin Buttons

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -2,7 +2,7 @@
 //  MapViewController.swift
 //  WorldTrotter
 //
-//  Created by Alex Louzao on 1/25/17.
+//  Created by Alex Louzao on 2/13/17.
 //  Copyright © 2017 ALcsc2310. All rights reserved.
 //
 
@@ -15,8 +15,7 @@ class MapViewController: UIViewController, MKMapViewDelegate{
     var mapView: MKMapView!
     var location: CLLocation!
     let locationManager = CLLocationManager()
-    
-    
+
     //self.mapView.delegate = self // where should this be in code -> loadView
     
     
@@ -34,6 +33,7 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         let pinButton: UIButton = UIButton(frame: CGRect(x:250, y:100, width: 100, height: 50))
         pinButton.setTitle("Get Pins", for: .normal)
         pinButton.backgroundColor = UIColor.blue
+        pinButton.tag = 1
         pinButton.addTarget(self, action: #selector(getPins), for: .touchUpInside)
         self.view.addSubview(pinButton)
         let locButton: UIButton = UIButton(frame: CGRect(x:16, y:100, width: 150, height: 50))
@@ -86,8 +86,6 @@ class MapViewController: UIViewController, MKMapViewDelegate{
         loadView()
     }
     
-    
-    
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation){
         //print("Getting User Location")
         let lat = mapView.userLocation.coordinate.latitude
@@ -103,37 +101,74 @@ class MapViewController: UIViewController, MKMapViewDelegate{
 
     }
     
-    func getPins(){
-        
-        let latitude1:CLLocationDegrees = 40.7684
-        let longitude1:CLLocationDegrees = -74.1454
-        
-        let latitude2:CLLocationDegrees = 35.9732
-        let longitude2:CLLocationDegrees = -79.9950
-        
-        let latitude3:CLLocationDegrees = 43.7332
-        let longitude3:CLLocationDegrees = -7.6740
-        
-        
+    func getPins(_ sender: UIButton){
         // have the button go through the pinned locations
-        print("Getting A Pin")
+        //print("Getting A Pin")
         
+        switch sender.tag{
+        case 1:
+            let lat:CLLocationDegrees = 40.7684
+            let long:CLLocationDegrees = -74.1454
+            let latDelta:CLLocationDegrees = 0.05
+            let longDelta:CLLocationDegrees = 0.05
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+            let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            
+            mapView.setRegion(region, animated: true)
+
+            let pin1 = MKPointAnnotation()
+            pin1.coordinate.latitude = lat
+            pin1.coordinate.longitude = long
+            pin1.title = "I Was Born Here"
+            mapView.addAnnotation(pin1)
+            sender.tag += 1
+            print(sender.tag)
+            break;
+        case 2:
+            let lat:CLLocationDegrees = 35.9732
+            let long:CLLocationDegrees = -79.9950
+            let latDelta:CLLocationDegrees = 0.05
+            let longDelta:CLLocationDegrees = 0.05
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+            let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            
+            mapView.setRegion(region, animated: true)
+            
+            let pin2 = MKPointAnnotation()
+            pin2.coordinate.latitude = lat
+            pin2.coordinate.longitude = long
+            pin2.title = "I Am Here"
+            mapView.addAnnotation(pin2)
+            sender.tag += 1
+            print(sender.tag)
+            break;
+        case 3:
+            let lat:CLLocationDegrees = 43.7332
+            let long:CLLocationDegrees = -7.6740
+            let latDelta:CLLocationDegrees = 0.05
+            let longDelta:CLLocationDegrees = 0.05
+            let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+            let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+            let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+            
+            mapView.setRegion(region, animated: true)
+           
+            let pin3 = MKPointAnnotation()
+            pin3.coordinate.latitude = lat
+            pin3.coordinate.longitude = long
+            pin3.title = "I Want To Be Here"
+            mapView.addAnnotation(pin3)
+            sender.tag += 1
+            break;
+        case 4:
+            mapView.showsUserLocation = true
+            sender.tag = 1
+        default: ()
+            break;
+        }
         
-        let pin1 = MKPointAnnotation()
-        let pin2 = MKPointAnnotation()
-        let pin3 = MKPointAnnotation()
-        pin1.coordinate.latitude = latitude1
-        pin1.coordinate.longitude = longitude1
-        pin2.coordinate.latitude = latitude2
-        pin2.coordinate.longitude = longitude2
-        pin3.coordinate.latitude = latitude3
-        pin3.coordinate.longitude = longitude3
-        pin1.title = "I Was Born Here"
-        pin2.title = "I Am Here"
-        pin3.title = "I Want To Be Here"
-        mapView.addAnnotation(pin1)
-        mapView.addAnnotation(pin2)
-        mapView.addAnnotation(pin3)
     }
     
     /*func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
